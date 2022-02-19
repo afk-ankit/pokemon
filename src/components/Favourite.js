0b8ccfa41db54a56e68ed8b4ffb93fc92ded8a4b
afk-ankit <ankitsharmagh093@gmail.com>
2022-02-19 14:21:19 +0530

    remove item functionality added

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -1,34 +1,27 @@
-import React, { useContext } from 'react'
-import FavouriteList from './FavouriteList'
+import React, { useContext } from "react";
+import FavouriteList from "./FavouriteList";
 import { useStateValue } from "./StateProvider";
 
 const Favourite = () => {
+  const [{ basket }, dispatch] = useStateValue();
 
-    const [{basket},dispatch]=useStateValue();
+  console.log(basket);
 
+  if (basket) {
+    return (
+      <>
+        {basket.map((item) => (
+          <FavouriteList item={item} key={Math.random() * 10} />
+        ))}
+      </>
+    );
+  } else if (basket.length != 0) {
+    return (
+      <div>
+        <h1>Please add in favourites to display your pokemon</h1>
+      </div>
+    );
+  }
+};
 
-    console.log(basket)
-
-  
-    if(basket.length>0)
-    {
-        return (
-            <>
-            {
-    
-             basket.map(item=><FavouriteList item={item}/>)
-            }
-            </>
-      )
-    }
-    else{
-        return(
-            <div>
-                <h1>Please add in favourites to display your pokemon</h1>
-            </div>
-        )
-    }
-
-}
-
-export default Favourite
+export default Favourite;
--- src/components/FavouriteList.js
+++ src/components/FavouriteList.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useStateValue } from "./StateProvider";
 
 const FavouriteList = ({ item }) => {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log(item.id);
+  let removefav = () => {
+    dispatch({
+      type: "REMOVE_BASKET",
+      data: {
+        name: item.name,
+        img: item.img,
+        id: item.id,
+      },
+    });
+  };
+
   return (
     <div className="card">
       <h1>{item.name}</h1>
+      <h3>{item.id}</h3>
       <img src={item.img} alt="" />
+      <button onClick={removefav}>Remove from fav</button>
     </div>
   );
 };
